36d74c1249ca1a14de661bd638da1aac3edef712
Seu Nome <escaldante333@gmail.com>
2025-02-16 17:41:18 -0300

    new commit

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
 fn main() {
-    let a = 10; // Tipo inferido como i32
-    let b = 20u8; // Tipo especificado como u8
-
-    let c = a + b as i32; // 'b' é convertido para i32 antes da adição
-
-    println!("c: {}", c);
+    let x: u16 = 500;
+    println!("x: {}", x);
 }
