8e5919fd1b7ee99421a374c724f9f8c0a25b03ec
Seu Nome <escaldante333@gmail.com>
2025-02-16 17:44:08 -0300

    new commit

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 fn main() {
-    let x: u16 = 500;
-    println!("x: {}", x);
+    let a = 10;
+    let b = 3;
+
+    println!("a + b = {}", a + b); // 13
+    println!("a - b = {}", a - b); // 7
+    println!("a * b = {}", a * b); // 30
+    println!("a / b = {}", a / b); // 3
+    println!("a % b = {}", a % b); // 1
 }
